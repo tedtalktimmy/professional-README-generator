7570bd4b48bd06ce1cb2655de8b161ca817bf091
tedtalktimmy <frtntn007@gmail.com>
2022-09-08 13:02:51 -0400

    created index and gitignore, installed inquirer

---
+++ index.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+// const util = require('util');
+
+inquirer.createPromptModule(
+    [
+        {
+            type:
+            message:
+            name: 
+        },
+        {
+            type:
+            message:
+            name: 
+        },
+        {
+            type:
+            message:
+            name:            
+        },
+        {
+            type:
+            message:
+            name: 
+        },
+        {
+            type:
+            message:
+            name: 
+        },
+        {
+            type:
+            message:
+            name: 
+        },
+        {
+            type:
+            message:
+            name: 
+        },
+        {
+            type:
+            message:
+            name: 
+        },
+        {
+            type:
+            message:
+            name:            
+        },
+        {
+            type:
+            message:
+            name: 
+        },
+        {
+            type:
+            message:
+            name: 
+        },
+        {
+            type:
+            message:
+            name: 
+        },
+    ]
+);
