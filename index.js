fac1f8681df74867035a771bea16e301eeaea9dd
tedtalktimmy <frtntn007@gmail.com>
2022-09-08 22:55:01 -0400

    completed set of questions

--- index.js
+++ index.js
@@ -14,8 +14,35 @@ prompt(
             name: 'description',
         },
         {
-            message: ' '
-            name:            
+            message: 'Would you like to provide instructions for using your application?',
+            name: 'use',
+        },
+        {
+            message: 'Please enter installation instructions.',
+            name: 'installation',
+        },
+        {
+            message: 'Please explain how other developers may contribute to your project.',
+            name: 'contributions',
+        },
+        {
+            message: "Please explain how to test your application.",
+            name: 'appTest',
+        },
+        {
+            type: 'checkbox',
+            message: 'Choose a license.',
+            name: 'license',
+            choices: [
+                'GNU AGPLv3',
+                'GNU GPLv3',
+                'GNU LGPLv3',
+                'Mozilla Public License 2.0',
+                'Apache License 2.0',
+                'MIT License',
+                'Boost Software License 1.0',
+                'The Unlicense'
+            ],
         },
         {
             message:
@@ -26,32 +53,12 @@ prompt(
             name: 
         },
         {
-            message:
-            name: 
+            message: 'Enter your github repository link.',
+            name: 'gitHubLink',
         },
         {
-            message:
-            name: 
-        },
-        {
-            message:
-            name: 
-        },
-        {
-            message:
-            name:            
-        },
-        {
-            message:
-            name: 
-        },
-        {
-            message:
-            name: 
-        },
-        {
-            message:
-            name: 
+            message: 'Enter your live site link.',
+            name: 'liveSite',
         },
     ]
 );
